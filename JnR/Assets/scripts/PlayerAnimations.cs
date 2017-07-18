88e5b030b0ed0acc9c5b9ea53eab13dc048b95e2
En0Ma1259 <f.boensch1@web.de>
2017-07-18 18:22:55 +0200

    JnR bewegung

---
+++ JnR/Assets/scripts/PlayerAnimations.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour {
+    public List<Texture2D> idleAnimationRight;
+    public List<Texture2D> idleAnimationLeft;
+    public List<Texture2D> runAnimationRight;
+    public List<Texture2D> runAnimationLeft;
+    public List<Texture2D> jumpAnimationRight;
+    public List<Texture2D> jumpAnimationLeft;
+
+    public float speed = 10;
+
+    public enum AniType
+    {
+        idleLeft,
+        idleRight,
+        runLeft,
+        runRight,
+        jumpLeft,
+        jumpRight
+    }
+
+    public AniType currAnimation = AniType.idleRight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (currAnimation) {
+            case AniType.idleLeft:
+                setAnimation(idleAnimationLeft);
+                break;
+            case AniType.idleRight:
+                setAnimation(idleAnimationRight);
+                break;
+            case AniType.runLeft:
+                setAnimation(runAnimationLeft);
+                break;
+            case AniType.runRight:
+                setAnimation(runAnimationRight);
+                break;
+            case AniType.jumpLeft:
+                setAnimation(jumpAnimationLeft);
+                break;
+            case AniType.jumpRight:
+                setAnimation(jumpAnimationRight);
+                break;
+        }
+	}
+
+    void setAnimation(List<Texture2D> listAnimations)
+    {
+        Renderer rend = GetComponent<Renderer>();
+        int index = (int)(Time.time * speed);
+        index %= listAnimations.Count;
+        rend.material.mainTexture = listAnimations[index];
+    }
+}
---
+++ JnR/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float gravity = 1;
+    float velocity;
+    public float speed = 5;
+    bool lookRight = true;
+    bool jump = false;
+    public float jumpFactor = 20;
+
+    public Vector3 moveDirection = Vector3.zero;
+    public CharacterController characterController;
+    public PlayerAnimations playerAnimations;
+
+    // Use this for initialization
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        playerAnimations = GetComponent<PlayerAnimations>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input();
+        moving();
+        setAnimation();
+    }
+
+    public void input()
+    {
+        velocity = Input.GetAxis("Horizontal") * speed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            jump = true;
+        else
+            jump = false;
+    }
+
+    public void moving()
+    {
+        if (characterController.isGrounded)
+        {
+            if (jump)
+            {
+                moveDirection.y = jumpFactor * Time.deltaTime;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        moveDirection.x = velocity * Time.deltaTime;
+        if (velocity > 0)
+            lookRight = true;
+        else if (velocity < 0)
+            lookRight = false;
+
+        characterController.Move(moveDirection);
+    }
+
+    public void setAnimation()
+    {
+        
+        if (velocity > 0)
+        {
+            playerAnimations.currAnimation = PlayerAnimations.AniType.runRight;
+            if (!characterController.isGrounded)
+                playerAnimations.currAnimation = PlayerAnimations.AniType.jumpRight;
+        }
+        if (velocity < 0)
+        {
+            playerAnimations.currAnimation = PlayerAnimations.AniType.runLeft;
+            if (!characterController.isGrounded)
+                playerAnimations.currAnimation = PlayerAnimations.AniType.jumpLeft;
+        }
+        if (velocity == 0)
+        {
+            if (lookRight)
+            {
+                playerAnimations.currAnimation = PlayerAnimations.AniType.idleRight;
+                if (!characterController.isGrounded) playerAnimations.currAnimation = PlayerAnimations.AniType.jumpRight;
+            }
+            else
+            {
+                playerAnimations.currAnimation = PlayerAnimations.AniType.idleLeft;
+                if (!characterController.isGrounded) playerAnimations.currAnimation = PlayerAnimations.AniType.jumpLeft;
+            } 
+        }
+        
+    }
+}
