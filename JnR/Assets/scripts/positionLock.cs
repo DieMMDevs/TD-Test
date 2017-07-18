fcc56e22e98f6f41d8af94cadbae8e66d33bf57e
En0Ma1259 <f.boensch1@web.de>
2017-07-18 15:02:15 +0200

    JnR

---
+++ JnR/Assets/scripts/positionLock.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class positionLock : MonoBehaviour {
+    public float axisOffset = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = transform.position;
+        pos.z = 0 + axisOffset;
+        transform.position = pos;
+	}
+}
