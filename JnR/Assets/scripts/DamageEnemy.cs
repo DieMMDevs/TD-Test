375be49f12c499a8d7629d430c1b89b1e19817d7
BuildTools <unconfigured@null.spigotmc.org>
2017-07-19 15:06:37 +0200

    schaden
    scheden nehmen
    gegner teilen schaden aus

---
+++ JnR/Assets/scripts/DamageEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageEnemy : MonoBehaviour
+{
+    public float damagevalue = 1;
+    public string tag = "Player";
+
+    public HealthController healthcontroller;
+    // Use this for initialization
+    void Start()
+    {
+        healthcontroller = new HealthController();
+        healthcontroller.DamageTaken(damagevalue);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == tag)
+        {
+            healthcontroller.DamageTaken(damagevalue);
+        }
+    }
+}
